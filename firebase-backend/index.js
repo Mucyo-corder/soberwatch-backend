0b0a2df1e07b2218ce7a70696ae04f8143405188
Mucyo-corder <mucyoepiphanie@gmail.com>
2026-08-25 12:30:16 +0300

    fix cors and doc()
    fix it

--- firebase-backend/index.js
+++ firebase-backend/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const admin = require("firebase-admin");
-const cors = require("cors"); // <-- Iki ni cyo cyongeweho kugira ngo App ibashe kuvugana na server!
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
-app.use(cors()); // Ibi birema browser/App gukora requests!
+app.use(cors());
 
 // ============================================================
 // FIREBASE INITIALIZATION
